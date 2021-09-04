9638347fd6e190bfc1055d1af458dae97a106376
Catherine Murphy <catherine_murphy@Catherines-MacBook-Pro.local>
2021-09-04 10:25:12 +0100

    Loaded example and input data

--- R/CHAS.R
+++ R/CHAS.R
@@ -95,10 +95,18 @@ plot_celltype_props <- function(annotatedPeaks, exampleData=TRUE){
   PropTable <- data.frame(prop.table(CellTable)*100)
   PropTable$Source <- "Peaks"
   if (exampleData == TRUE) {
-    PropTable$Var1 <- factor(PropTable$Var1, levels = c("Astrocytes", "Microglia", "Neurons", "Oligodendrocytes", "Multiple", "Other"))
-    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) + geom_bar(stat="identity") + theme_classic() + scale_fill_manual(values=c("#446455", "#FDD262", "#46ACC8", "#F4B5BD", "#E58601", "#B40F20")) + ylab("Proportion of peaks specific to each cell type (%)") + xlab("Peaks") + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + guides(fill=guide_legend(title="Cell type"))
+    PropTable$Var1 <- factor(PropTable$Var1, levels = c("Astrocyte", "Microglia", "Neuron", "Oligodendrocyte", "Multiple", "Other"))
+    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
+      geom_bar(stat="identity") + theme_classic() + scale_fill_manual(values=c("#446455", "#FDD262", "#46ACC8", "#F4B5BD", "#E58601", "#B40F20")) +
+      ylab("Proportion of peaks specific to each cell type (%)") +
+      xlab("Peaks") + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+      guides(fill=guide_legend(title="Cell type"))
   } else {
-    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) + geom_bar(stat="identity") + theme_classic() + scale_fill_brewer(palette="Accent") + ylab("Proportion of peaks specific to each cell type (%)") + xlab("Peaks") + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + guides(fill=guide_legend(title="Cell type"))
+    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
+      geom_bar(stat="identity") + theme_classic() + scale_fill_brewer(palette="Accent") +
+      ylab("Proportion of peaks specific to each cell type (%)") + xlab("Peaks") +
+      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+      guides(fill=guide_legend(title="Cell type"))
   }
   return(plot)
 }
@@ -113,9 +121,12 @@ plot_celltype_props <- function(annotatedPeaks, exampleData=TRUE){
 #' @export
 plot_celltype_scores <- function(celltypeScores, pheno){
 
-  annotScores <- merge(celltypeScores, AD_pheno, by="Sample")
-
-  plot <- ggplot(annotScores, aes(Celltype, Score, fill=Group)) + geom_violin() + labs(x="Cell type", y="Cell type score") + theme_classic() + scale_fill_manual(values = wes_palette("Darjeeling2"))
+  annotScores <- merge(celltypeScores, pheno, by="Sample")
+  maxScore = max(annotScores$Score)
+  plot <- ggplot(annotScores, aes(Celltype, Score, fill=Group)) +
+    geom_violin() + labs(x="Cell type", y="Cell type score") +
+    theme_classic() + scale_fill_manual(values = wes_palette("Darjeeling2")) +
+    stat_compare_means(method="t.test", aes(label = paste0("p = ", ..p.format..)), label.y=(maxScore+0.025))
 
   return(plot)
 }
