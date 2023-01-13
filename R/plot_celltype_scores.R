f414c15437590268240d73f4b77cff0b76bd76fa
Kitty Murphy <56632280+KittyMurphy@users.noreply.github.com>
2023-01-13 14:38:58 +0000

    Create plot_celltype_scores.R

---
+++ R/plot_celltype_scores.R
@@ -0,0 +1,20 @@
+#' Plot cell type scores between two groups
+#'
+#' This function takes as input the cell type scores generated using the function CelltypeScore()
+#' and plots a violin plot of the cell type scores between two different groups.
+#' @param celltypeScores The output data frame from the function CelltypeScore()
+#' @param pheno A data frame containing a column named Sample and a column named Group.
+#' @return A violin plot showing the cell type scores between two different groups.
+#' @import ggplot2
+#' @export
+plot_celltype_scores <- function(celltypeScores, pheno){
+
+  annotScores <- merge(celltypeScores, pheno, by="Sample")
+  maxScore = max(annotScores$Score)
+  plot <- ggplot(annotScores, aes(Celltype, Score, fill=Group)) +
+    geom_violin() + labs(x="Cell type", y="Cell type score") +
+    theme_classic() + scale_fill_manual(values = wes_palette("Darjeeling2")) +
+    stat_compare_means(method="t.test", aes(label = paste0("p = ", ..p.format..)), label.y=(maxScore+0.025))
+
+  return(plot)
+}
