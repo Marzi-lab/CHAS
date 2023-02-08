4db9eaeefd82e0d86c5873937edd9494d3e22bbb
YuqianY <yuqian.ye.uk@gmail.com>
2023-02-08 16:32:22 +0000

    changed colour palette

--- R/plot_celltype_props.R
+++ R/plot_celltype_props.R
@@ -7,24 +7,20 @@
 #' @return A stacked bar plot showing the proportion of each cell type in the bulk peak set.
 #' @import ggplot2
 #' @export
-plot_celltype_props <- function(annotatedPeaks, exampleData=TRUE){
+plot_celltype_props <- function(annotatedPeaks){
   allPeaks = annotatedPeaks[[2]]
   CellTable <- table(allPeaks$Annot)
   PropTable <- data.frame(prop.table(CellTable)*100)
   PropTable$Source <- "Peaks"
-  if (exampleData == TRUE) {
-    PropTable$Var1 <- factor(PropTable$Var1, levels = c("Astrocyte", "Microglia", "Neuron", "Oligodendrocyte", "Multiple", "Other"))
-    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
-      geom_bar(stat="identity") + theme_classic() + scale_fill_manual(values=c("#446455", "#FDD262", "#46ACC8", "#F4B5BD", "#E58601", "#B40F20")) +
-      ylab("Proportion of peaks specific to each cell type (%)") +
-      xlab("Peaks") + theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-      guides(fill=guide_legend(title="Cell type"))
-  } else {
-    plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
-      geom_bar(stat="identity") + theme_classic() + scale_fill_brewer(palette="Accent") +
-      ylab("Proportion of peaks specific to each cell type (%)") + xlab("Peaks") +
-      theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-      guides(fill=guide_legend(title="Cell type"))
-  }
+  PropTable$Var1 <- factor(PropTable$Var1,
+                           levels = c(sort(unique(annotatedPeaks[[2]]$Annot)[unique(annotatedPeaks[[2]]$Annot) != c("Multiple", "Other")])
+                                           , "Multiple", "Other"))
+  plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
+    geom_bar(stat="identity") + theme_classic() +
+    scale_fill_manual(values=c("#446455", "#FDD262", "#46ACC8", "#F4B5BD",
+                               brewer.pal(7, "Accent")[0:(nrow(PropTable)-6)],"#E58601","#DDDDDD")) +
+    ylab("Proportion of peaks specific to each cell type (%)") + xlab("Peaks") +
+    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
+    guides(fill=guide_legend(title="Cell type"))
   return(plot)
 }
