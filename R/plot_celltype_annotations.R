edbb816960f2f51571b3851ae7760e869fbad26a
Kitty Murphy <c.murphy19@imperial.ac.uk>
2023-02-16 17:04:43 +0000

    code fix to cell type annotation plot

--- R/plot_celltype_annotations.R
+++ R/plot_celltype_annotations.R
@@ -9,14 +9,14 @@
 #' @import RColorBrewer
 #' @export
 
-plot_celltype_props <- function(annotatedPeaks){
+plot_celltype_annotations <- function(annotatedPeaks){
   allPeaks = annotatedPeaks[[2]]
   CellTable <- table(allPeaks$Annot)
   PropTable <- data.frame(prop.table(CellTable)*100)
   PropTable$Source <- "Peaks"
   PropTable$Var1 <- factor(PropTable$Var1,
-                           levels = c(sort(unique(annotatedPeaks[[2]]$Annot)[unique(annotatedPeaks[[2]]$Annot) != c("Multiple", "Other")])
-                                           , "Multiple", "Other"))
+                           c(sort(unique(allPeaks$Annot[!allPeaks$Annot=="Multiple" & !allPeaks$Annot=="Other"])),
+                             "Multiple", "Other"))
   plot <- ggplot(PropTable, aes(x=Source, y=Freq, fill=Var1)) +
     geom_bar(stat="identity") + theme_classic() +
     scale_fill_manual(values=c("#446455", "#FDD262", "#46ACC8", "#F4B5BD",
