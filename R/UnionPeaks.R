f62462b524c7b03503cd9f79b2e4199c70502fdb
Kitty Murphy <cm1118@ic.ac.uk>
2024-07-05 14:49:11 +0100

    Generalise chr ordering

--- R/UnionPeaks.R
+++ R/UnionPeaks.R
@@ -28,46 +28,42 @@
 #'   the second data frame contains the cell type-specific peaks
 #' @export
 
-UnionPeaks <- function(bulkPeaks,refPeakList,bedtools_path){
+UnionPeaks <- function(bulkPeaks, refPeakList, bedtools_path) {
 
-  # merge all bulk & ref peaks
+  # Merge all bulk and ref peaks
   merPeaks <- bulkPeaks
-  row.names(merPeaks) = NULL
+  row.names(merPeaks) <- NULL
   merPeaks[4] <- paste0("bulk_peak_", row.names(merPeaks))
   names(merPeaks) <- names(refPeakList[[1]])
-  for (x in names(refPeakList)){
-    refPeakList[[x]][4] <-paste0(x,"_peak_",row.names(refPeakList[[x]]))
+  for (x in names(refPeakList)) {
+    refPeakList[[x]][4] <- paste0(x, "_peak_", row.names(refPeakList[[x]]))
     merPeaks <- rbind(merPeaks, refPeakList[[x]])
   }
 
-  # sort the peaks by chromosome number, then by start location
-  merPeaks$No <- sub("chr", "", merPeaks[,1])
-  merPeaks$No <- stringi::stri_replace_all_regex(merPeaks$No,
-                 pattern = c("X","Y"), replacement = c("23", "24"), vectorize=FALSE)
-  merPeaks$No <- as.numeric(merPeaks$No)
-  merPeaks <- merPeaks[order(merPeaks$No, merPeaks$Start),]
+  # Sort the peaks by chromosome name, then by start location
+  merPeaks <- merPeaks[order(merPeaks[,1], as.numeric(merPeaks[,2])),]
   merPeaks <- merPeaks[,1:4]
-  write.table(merPeaks, 'merPeaks.bed', row.names = FALSE, col.names = FALSE, sep = '\t', quote=FALSE)
+  write.table(merPeaks, 'merPeaks.bed', row.names = FALSE, col.names = FALSE, sep = '\t', quote = FALSE)
 
-  # use bedtools merge to get the union peaks
-  system(paste("cd",getwd()))
-  system(paste(bedtools_path,"merge -i merPeaks.bed -c 4 -o distinct -d 20 > unionPeaks.bed"))
+  # Use bedtools merge to get the union peaks
+  system(paste("cd", getwd()))
+  system(paste(bedtools_path, "merge -i merPeaks.bed -c 4 -o distinct -d 20 > unionPeaks.bed"))
   unionPeaks <- read.table("unionPeaks.bed")
 
-  # make .saf file for read counting later
+  # Make .saf file for read counting later
   unionPeaks_saf <- unionPeaks
   unionPeaks_saf$V5 <- "."
   unionPeaks_saf$V1 <- paste(unionPeaks$V1, unionPeaks$V2, unionPeaks$V3, sep = ".")
   unionPeaks_saf[2:4] <- unionPeaks[1:3]
   readr::write_tsv(unionPeaks_saf, "unionPeaks.saf", col_names = FALSE)
 
-  # identify cell-type-specific peaks
+  # Identify cell-type-specific peaks
   celltypePeaks <- unionPeaks
   for (x in c(names(refPeakList), "bulk")) {
-    celltypePeaks[x]  <- grepl(x, celltypePeaks$V4, fixed = TRUE)
+    celltypePeaks[x] <- grepl(x, celltypePeaks$V4, fixed = TRUE)
   }
-  celltypePeaks$celltype <- apply(celltypePeaks[,5:(length(names(refPeakList))+4)], 1, sum)
-  celltypePeaks <- celltypePeaks[celltypePeaks$celltype==1 & celltypePeaks$bulk==TRUE,]
+  celltypePeaks$celltype <- apply(celltypePeaks[,5:(length(names(refPeakList)) + 4)], 1, sum)
+  celltypePeaks <- celltypePeaks[celltypePeaks$celltype == 1 & celltypePeaks$bulk == TRUE,]
 
-  return(list(unionPeaks=unionPeaks, celltypePeaks=celltypePeaks))
+  return(list(unionPeaks = unionPeaks, celltypePeaks = celltypePeaks))
 }
